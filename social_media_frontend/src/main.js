0179927fbccee802e1ff101116c824a17f1b1d9c
Mutlucan Gökçukur <mtlcangokcukur@hotmail.com>
2025-01-18 18:05:35 +0300

    Axios installed and added the proejct

--- social_media_frontend/src/main.js
+++ social_media_frontend/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import appAxios from './utils/appAxios';
 
 const app = createApp(App)
+app.provide('appAxios', appAxios);
 app.use(router)
 app.mount('#app')
---
+++ social_media_frontend/src/utils/appAxios.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { apiBaseURL } from '@/src/config/config';   //* Retrieves API base URL configuration settings
+
+const appAxios = axios.create({
+    baseURL: `${apiBaseURL.host}:${apiBaseURL.port}/`,
+    timeout: 10000,
+});
+
+export default appAxios;
+
+
+/*#region Gerekli olursa kullanılacak olan Content-Type ve Accent değerleri
+
+appAxios.defaults.headers['Content-Type'] = 'application/json';
+appAxios.defaults.headers['Accept'] = 'application/json';
+
+#endregion*/
+
+/*#region Her istekte token ekleme 
+
+appAxios.interceptors.request.use(
+    config => 
+    {
+        const token = localStorage.getItem('token');
+        if (token) 
+        {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => 
+    {
+        return Promise.reject(error);
+    }
+);
+
+#endregion*/
